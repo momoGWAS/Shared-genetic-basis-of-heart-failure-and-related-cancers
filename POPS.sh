fb67cd81214608d11afb60f9e1276eccf7381006
momoGWAS <154742625+momoGWAS@users.noreply.github.com>
2025-11-02 20:45:59 +0800

    Update and rename POPS循环.sh to POPS.sh

--- POPS.sh
+++ POPS.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
-# FUMA文件夹的路径
+
 FUMA_DIR="./FUMA"
-
-# 结果文件夹的路径
-OUT_DIR="./结果"
-
-# 创建结果文件夹如果它不存在的话
+OUT_DIR="./result"
 mkdir -p "$OUT_DIR"
 
-# 遍历FUMA文件夹下的每个子文件夹
+
 for SUBDIR in "$FUMA_DIR"/*; do
   if [ -d "$SUBDIR" ]; then
-    # 获取子文件夹的名字
+   
     SUBDIR_NAME=$(basename "$SUBDIR")
-    # 构建输出前缀
+ 
     OUT_PREFIX="$OUT_DIR/$SUBDIR_NAME"
-    # 运行pops.py脚本
+ 
     python pops.py --gene_annot_path ./gene_all.txt \
                    --feature_mat_prefix ./features_munged/pops_features \
                    --num_feature_chunks 2 \
